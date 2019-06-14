529b1fd0c5100f5aa1a46afe12fbb74ab5e8ea55
Thomas Frekhaug <95frekhaug@gmail.com>
2019-06-14 16:50:27 +0200

    Test of writing to EEPROM

---
+++ kode/Emwrite_test/Emwrite_test.ino
@@ -0,0 +1,20 @@
+#include"temp.h"
+
+class tempsensor tmp;
+
+
+void setup() {
+  tmp.begin();
+  Serial.begin(9600);
+}
+
+void loop() {
+
+  Serial.print("temp_amb,");
+  Serial.print(tmp.get_AMB_tmp());
+  Serial.print(",temp_obj,");
+  Serial.println(tmp.get_OBJTOT_tmp());
+      tmp.set_emissivity(73);
+
+  delay(1000);
+}
---
+++ kode/Emwrite_test/temp.h
@@ -0,0 +1,36 @@
+#include <Wire.h>
+#if (ARDUINO >= 100)
+ #include "Arduino.h"
+#else
+ #include "WProgram.h"
+#endif
+
+#define MLX90615_I2C_ADDR    0x5A
+//RAM ACCESS
+#define RAM_RAW_DATA_IR_CHANNEL_1 	0x04
+#define RAM_RAW_DATA_IR_CHANNEL_2 	0x05
+#define RAM_TA                    	0x06
+#define RAM_TOBJ1                 	0x07 
+#define RAM_TOBJ2                 	0x08 
+
+//EPROM ACCESS
+#define EPROOM_MAX					0x20
+#define EPROOM_MIN 					0x21
+#define EPROOM_TA_RANGE 			0x23
+#define EPROOM_PWM_CONTROL 			0x22
+#define EPROOM_EMMISSION 			0x24
+
+class tempsensor{
+	private: 
+		uint16_t read_word16(uint8_t reg);
+		void  write_word16();
+	public: 
+
+		void  begin();
+		float get_AMB_tmp();
+		float get_OBJ1_tmp();
+		float get_OBJ2_tmp();
+		float get_OBJTOT_tmp();
+		void set_emissivity(int em);
+		
+};
