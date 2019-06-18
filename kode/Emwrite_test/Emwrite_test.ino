5158fe721a75d7e30327a362a6e84a65078e40e3
Thomas Frekhaug <95frekhaug@gmail.com>
2019-06-18 19:15:37 +0200

    updated drivers

--- kode/Emwrite_test/Emwrite_test.ino
+++ kode/Emwrite_test/Emwrite_test.ino
@@ -1,6 +1,6 @@
-#include"temp.h"
+#include"temp_IR.h"
 
-class tempsensor tmp;
+class temp_IR tmp;
 
 
 void setup() {
@@ -14,7 +14,7 @@ void loop() {
   Serial.print(tmp.get_AMB_tmp());
   Serial.print(",temp_obj,");
   Serial.println(tmp.get_OBJTOT_tmp());
-      tmp.set_emissivity(73);
+      //tmp.set_emissivity(73);
 
   delay(1000);
 }
--- kode/Emwrite_test/temp_IR.cpp
+++ kode/Emwrite_test/temp_IR.cpp
@@ -1,4 +1,4 @@
- #include "temp.h"
+ #include "temp_IR.h"
 
 byte crc8(byte *addr, byte len)
 {
@@ -18,7 +18,7 @@ byte crc8(byte *addr, byte len)
 } 
 
 
-void tempsensor::write_word16(){
+void temp_IR::write_word16(){
   uint8_t i2c_addr_ = MLX90615_I2C_ADDR;
   byte buf [] = {0x00, 0x24,0x00,0x00  };
   byte crc = crc8 (buf, sizeof buf);
@@ -36,21 +36,21 @@ void tempsensor::write_word16(){
   delay(1000);
 
 
-  byte bu2 [] = {0x00, 0x24,0xBB,0x00  };
+  byte bu2 [] = {0x00, 0x24,0xFF,0xFF  };
   crc = crc8 (bu2, sizeof bu2);
   Serial.print ("CRC was: ");
   Serial.println (crc, HEX);   
 
   Wire.beginTransmission(0x00);
   Wire.write(0x24); //Register Address to write to
-  Wire.write(0xBB); //Erase low byte (write 0)
-  Wire.write(0x00); //Erase high byte (write 0)1
+  Wire.write(0xFF); //Erase low byte (write 0)
+  Wire.write(0xFF); //Erase high byte (write 0)1
   Wire.write(crc); //Erase high byte (write 0)1
   Serial.println( Wire.endTransmission() );
   delay(1000);
 }
 
-void tempsensor::set_emissivity(int em){
+void temp_IR::set_emissivity(int em){
   write_word16();
   Serial.print("Emissivity: ");
   Serial.println(read_word16(EPROOM_EMMISSION),HEX);
@@ -59,28 +59,28 @@ void tempsensor::set_emissivity(int em){
 
 }
 
-void tempsensor::begin() {
+void temp_IR::begin() {
   Wire.begin();
 }
 
-float tempsensor::get_AMB_tmp(){
+float temp_IR::get_AMB_tmp(){
 	return read_word16(RAM_TA)*0.02 - 273.15;
 }
 
-float tempsensor::get_OBJ1_tmp(){
+float temp_IR::get_OBJ1_tmp(){
 	return read_word16(RAM_TOBJ1)*0.02 - 273.15;
 }
 
-float tempsensor::get_OBJ2_tmp(){
+float temp_IR::get_OBJ2_tmp(){
 	return read_word16(RAM_TOBJ2)*0.02 - 273.15;
 }
 
-float tempsensor::get_OBJTOT_tmp(){
+float temp_IR::get_OBJTOT_tmp(){
 	float obj1 = read_word16(RAM_TOBJ1);
 	float obj2 = read_word16(RAM_TOBJ2);
 	return (((obj1 + obj2)/2)*0.02 - 273.15); 
 }
-uint16_t tempsensor::read_word16(uint8_t reg){
+uint16_t temp_IR::read_word16(uint8_t reg){
   uint8_t i2c_addr_ = MLX90615_I2C_ADDR;
   uint16_t data, LSB, MSB;
   Wire.beginTransmission(i2c_addr_);
