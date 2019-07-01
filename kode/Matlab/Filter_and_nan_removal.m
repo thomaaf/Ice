3c1d869fd0019fae394821b02ebbdc9d9803d76c
unknown <95frekhaug@gmail.com>
2019-07-01 10:32:48 +0200

    meh

---
+++ kode/Matlab/Filter_and_nan_removal.m
@@ -0,0 +1,28 @@
+RTD_filtered = zeros(size(S.millis,2),1);
+alfa = 0.05;
+for i = 2:size(S.millis,2)
+    if isnan(S.RTD(i))
+        S.RTD(i) = S.RTD(i-1);
+    end
+    if isnan(S.currentEst(i)) || isnan(S.millis(i))
+       disp (i)
+       disp("Current");
+       disp(S.currentEst(i));
+       disp("millis");
+       disp(S.millis(i));
+       
+    end
+    RTD_filtered(i) = RTD_filtered(i-1) + alfa*(S.RTD(i-1) - RTD_filtered(i-1));
+    
+end
+%%
+S.RTD = S.RTD(1:89210);
+S.millis = S.millis(1:89210);
+S.currentEst = S.currentEst(1:89210);
+S.currentMeas = S.currentMeas(1:89210);
+%%
+figure(1)
+clf(1)
+plot(S.RTD)
+hold on
+plot(RTD_filtered)
